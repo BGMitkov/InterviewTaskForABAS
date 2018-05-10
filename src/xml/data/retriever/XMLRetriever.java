493c4cece076888a6a3f662f26124384a59d779b
bgmitkov <ropsan@abv.bg>
2018-05-10 12:47:53 +0300

    a working code for the first Task

--- src/xml/data/retriever/XMLRetriever.java
+++ src/xml/data/retriever/XMLRetriever.java
@@ -3,6 +3,7 @@ package xml.data.retriever;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+
 import javax.xml.namespace.QName;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
@@ -10,6 +11,7 @@ import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.Attribute;
 import javax.xml.stream.events.Characters;
+import javax.xml.stream.events.EndElement;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 
@@ -34,32 +36,27 @@ public class XMLRetriever {
 				switch(event.getEventType()) {
 				case XMLStreamConstants.START_ELEMENT :
 					StartElement startElement = event.asStartElement();
-					System.out.println(startElement.getName());
 					if(startElement.getName().getLocalPart().equals(tagName)) {
 						Attribute attributeByName = startElement.getAttributeByName(QName.valueOf(attributeName));
-						while(xmlEventReader.hasNext() && !xmlEventReader.peek().isCharacters())
 						if(attributeByName.getValue().equals(attribute)) {
 							result.append(attributeByName.getValue());
 							result.append(": ");
-							if(xmlEventReader.peek().isCharacters()) {
-								tagFound = Boolean.TRUE;
-								System.out.println("Tag found");
-							} else {
-								result.append(System.lineSeparator());
-							}
+							tagFound = Boolean.TRUE;
 						}
 					}
 					break;
 				case XMLStreamConstants.CHARACTERS:
 					if(tagFound.equals(Boolean.TRUE)) {
-						System.out.println("data found");
 						Characters chars = event.asCharacters();
 						result.append(chars.getData());
 					}
 					break;
 				case XMLStreamConstants.END_ELEMENT:
-					tagFound = Boolean.FALSE;
-					result.append(System.lineSeparator());
+					EndElement endElement = event.asEndElement();
+					if(endElement.getName().getLocalPart().equals(tagName) && tagFound.equals(Boolean.TRUE)) {
+						tagFound = Boolean.FALSE;
+						result.append(System.lineSeparator());
+					}
 				default:break;
 				};
 				
